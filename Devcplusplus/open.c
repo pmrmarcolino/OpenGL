2986efa730007128e8323fd0c30abe3789912ca5
Patricia Marcolino <pmrmarcolino@hotmail.com>
2017-09-19 17:23:59 -0300

    Update open.c

--- Devcplusplus/open.c
+++ Devcplusplus/open.c
@@ -1,64 +1,87 @@
 #include "open.h"
 
-Quadrado *InitQuad(Pontos *p){
+#define tam 1
+
+Pontos *InitQuad(Pontos *p){
 	
 	//quadrado tem 4 pontos.
 	p = (Pontos*) malloc (4* sizeof(Pontos));
 	
-	p[0].x = -0.5;
-	p[0].y = -0.5;		
+	p[0].x = -0.1f;
+	p[0].y = -0.1f;		
 	
-	p[1].x =  0.5;
-	p[1].y = -0.5;	
+	p[1].x =  0.1f;
+	p[1].y = -0.1f;	
 		
-	p[2].x =  0.5;
-	p[2].y =  0.5;
+	p[2].x =  0.1f;
+	p[2].y =  0.1f;
 	
-	p[3].x = -0.5;
-	p[3].y =  0.5;
+	p[3].x = -0.1f;
+	p[3].y =  0.1f;
+
 	
-	Quadrado *q = (Quadrado*) malloc (sizeof(Quadrado));
-	q->ponto = p;
-	
-	return q;  
+	return p;  
 }
 
-Quadrado** alocaMat(Quadrado *quad , Quadrado **Mat){
+Quadrado** alocaMat(Quadrado *quad, Quadrado **Mat){
    Mat = (Quadrado **) malloc (tam * sizeof (Quadrado*));
    for (int i = 0; i < tam; ++i)
    	 	Mat[i] =(Quadrado *) malloc (tam * sizeof (Quadrado));
-   	 	
-   	for(int l = 0; l<tam; l++){
+
+	for(int l = 0; l<tam; l++){
 		for(int c = 0; c< tam; c++){
 			Mat[l][c].ponto = quad->ponto;
-			//printf("%f %f ", Mat[l][c].ponto[0].x, Mat[l][c].ponto[0].y);				
+			}
 		}
-	}
+   	
 	return Mat;
 }
 
 Quadrado ** preencheMat(Quadrado **M, Pontos *p){
-	double dist = pow(pow(abs(p[0].x - p[1].y),2) + abs(pow(abs(p[0].y - p[1].y),2)),1/2);
-
+	float dist = pow(pow(abs(p[2].x - p[3].y),2) + abs(pow(abs(p[2].y - p[3].y),2)),1/2);
+	int c;
 		for(int l = 0; l<tam; l++){
-			for(int c = 0; c< tam; c++){
-				printf("%d quadrado\n\n",l+c);
+			
+			for(c = 0; c< tam; c++){
+				printf("\n%d quadrado\n\n",c);
 				
-					M[l][c].ponto[0].x += (dist*l);
-					M[l][c].ponto[0].y += (dist*l);
-					printf("1:%f %f \t", M[l][c].ponto[0].x, M[l][c].ponto[0].y);				
+				if (c == 0 && l == 0){}		
+				else{
 					
-					M[l][c].ponto[1].x += (dist*l);
-					M[l][c].ponto[1].y += (dist*l);
-					printf("2:%f %f\t", M[l][c].ponto[1].x, M[l][c].ponto[1].y);				
+					M[l][c].ponto[0].x = M[l][c-1].ponto[0].x + dist;
+					M[l][c].ponto[0].y = M[l][c].ponto[0].y;				
+					printf("x0: %f y0: %f \t", M[l][c].ponto[0].x, M[l][c].ponto[0].y);				
 					
-					M[l][c].ponto[2].x += (dist*c);
-					M[l][c].ponto[2].y += (dist*c);
-					printf("3:%f %f\t", M[l][c].ponto[2].x, M[l][c].ponto[2].y);				
+					M[l][c].ponto[1].x = M[l][c-1].ponto[1].x + dist;
+					M[l][c].ponto[1].y = M[l][c].ponto[1].y;
+					printf("x1: %f y0: %f\t", M[l][c].ponto[1].x,M[l][c].ponto[1].y);				
 					
-					M[l][c].ponto[3].x += (dist*c);
-					M[l][c].ponto[3].y += (dist*c);
-					printf("4:%f %f\n", M[l][c].ponto[3].x, M[l][c].ponto[3].y);						
+					M[l][c].ponto[2].x = M[l][c-1].ponto[2].x + dist;
+					M[l][c].ponto[2].y = M[l][c].ponto[2].y;
+					printf("x2: %f y0: %f\t", M[l][c].ponto[2].x, M[l][c].ponto[1].y);				
+					
+					M[l][c].ponto[3].x = M[l][c-1].ponto[3].x + dist;
+					M[l][c].ponto[3].y = M[l][c].ponto[3].y;
+					printf("x3: %f y0: %f\n", M[l][c].ponto[3].x, M[l][c].ponto[1].y);						
+				}
+			}
+			if (!(c == 0 && l == 0)){
+			
+				M[l][c].ponto[0].y = M[l-1][c].ponto[0].y + dist;
+				M[l][c].ponto[0].x = M[l][c].ponto[0].x;
+				printf("y0: %f \t", M[l][c].ponto[0].y);	
+				
+				M[l][c].ponto[1].y = M[l-1][c].ponto[1].y + dist;
+				M[l][c].ponto[1].x = M[l][c].ponto[1].x;
+				printf("y1: %f \t", M[l][c].ponto[0].y);	
+				
+				M[l][c].ponto[2].y = M[l-1][c].ponto[2].y + dist;
+				M[l][c].ponto[2].x = M[l][c].ponto[2].x;
+				printf("y2: %f \t", M[l][c].ponto[0].y);	
+				
+				M[l][c].ponto[3].y = M[l-1][c].ponto[3].y + dist;
+				M[l][c].ponto[3].x = M[l][c].ponto[3].x;
+				printf("y3: %f \t", M[l][c].ponto[0].y);	
 			}
 		}
 	return M;
@@ -70,7 +93,10 @@ void Desenha(void){
 	 Quadrado *quad;
 	 Quadrado ** Mat;
 	 
-	 quad = InitQuad(p);
+	 p = InitQuad(p);
+	 
+	quad = (Quadrado*) malloc (sizeof(Quadrado));
+	quad->ponto = p;
 	 
 	 Mat = alocaMat(quad,Mat);
 	 	 
@@ -79,7 +105,7 @@ void Desenha(void){
 	glClearColor(1,1,1,0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1,0,0);
-	glBegin(GL_QUAD_STRIP);
+	glBegin(GL_QUADS);
 		for (int i = 0; i<tam; i++){
 			for(int j =0; j<tam;j++ ){
 				glVertex2f(Mat[i][j].ponto[0].x, Mat[i][j].ponto[0].y);
@@ -89,7 +115,7 @@ void Desenha(void){
 			}
 		}
 	glEnd();
-	glFlush(); //  força as execuções do GL em um tempo finito pra um buffer
+	glFlush(); //  forÃ§a as execuÃ§Ãµes do GL em um tempo finito pra um buffer
 	glutSwapBuffers(); // pra mais de um buffer
 }
 
@@ -105,7 +131,7 @@ void Teclado (unsigned char key, int x, int y){
 
 void Inicializa(void){
 	glMatrixMode(GL_PROJECTION);
-	gluOrtho2D(-1,1,-1,1);// posição do desenho na tela, centro no caso
+	gluOrtho2D(-1,1,-1,1);// posiÃ§Ã£o do desenho na tela, centro no caso
 	glMatrixMode(GL_MODELVIEW);
 }
 
@@ -123,4 +149,3 @@ void liberaMatriz(Quadrado ** Mat){
 		}
 		free(Mat);		
 }
-
