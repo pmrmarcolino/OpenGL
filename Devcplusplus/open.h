447b3b9b990f97e79b55c6d67501192f479e97b6
Patricia Marcolino <pmrmarcolino@hotmail.com>
2017-10-02 16:41:24 -0300

    Add files via upload
    A gráfica não está lendo.

---
+++ Devcplusplus/open.h
@@ -0,0 +1,24 @@
+#include <GL/glut.h> // Responsavel por gerenciamento de janelas e tratamento de eventos
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <conio.h>
+
+typedef struct ponto{
+	float x;
+	float y;
+	float z;
+}Pontos;
+
+typedef struct quad{
+	Pontos *ponto;
+}Triangulo;
+
+Triangulo** alocaMat( Triangulo **Mat);
+Triangulo ** preencheMat(Triangulo **M);
+void Desenha(void);
+void Teclado (unsigned char key, int x, int y);
+void Inicializa(void);
+void Tela(void);
+void PreencheZero(Triangulo ** Mat);
+void imprimeMat (Triangulo ** Mat);
